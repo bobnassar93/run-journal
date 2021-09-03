aa25e488adda95ce5163d756aa98e9d7d5906ab5
Bob Nassar <nassarbob@outlook.com>
2021-09-04 00:18:42 +0300

    Minor fix

--- src/app/shared/activities.ts
+++ src/app/shared/activities.ts
@@ -5,7 +5,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
         id: 1,
         name: 'Main Bike Trails',
         date: new Date('09/03/2021'),
-        gpxData: '/assets/gpx/1.gpx',
+        gpxData: 'assets/gpx/1.gpx',
         distance: 16.2,
         comments: 'Nice day, cool temps'
     },
@@ -13,7 +13,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
         id: 2,
         name: 'Industrial Park',
         date: new Date('06/04/2017'),
-        gpxData: '/assets/gpx/1.gpx',
+        gpxData: 'assets/gpx/1.gpx',
         distance: 1.2,
         comments: 'Cool and windy.'
     },
@@ -21,7 +21,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
         id: 3,
         name: 'Forest Route',
         date: new Date('06/05/2017'),
-        gpxData: '/assets/gpx/2.gpx',
+        gpxData: 'assets/gpx/2.gpx',
         distance: 3.2,
         comments: 'Evening run.'
     },
@@ -29,7 +29,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
         id: 4,
         name: 'Lake Shore',
         date: new Date('06/08/2017'),
-        gpxData: '/assets/gpx/4.gpx',
+        gpxData: 'assets/gpx/4.gpx',
         distance: 8.4,
         comments: 'Cool and windy by the lake.'
     }
