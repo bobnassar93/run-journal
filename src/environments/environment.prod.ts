7cb47490e4a8643e4a513d63115ffcd1bbc45bb4
Bob Nassar <nassarbob@outlook.com>
2021-09-04 00:06:13 +0300

    Added prod files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  MAPBOX_API_KEY: 'pk.eyJ1IjoibmFzc2FyYm9iIiwiYSI6ImNrdDR0Y2t6ZTAxeXAyb245NDUxd3J2Z2cifQ.GeT5CqfYf3FeUti0IJpjGg'
 };
